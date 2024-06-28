0adeff9db8c51532650433b472177159492ded2c
Maria Panfilova <travailpart@gmail.com>
2024-06-28 17:39:22 +0400

    swath_resample and create_mask functions added

---
+++ src/swath_processing.py
@@ -0,0 +1,68 @@
+from omegaconf import DictConfig
+from .annotation import ExtractedHDF5Data
+from .defs import FILL_VALUE_RESAMPLE, INFL_RADIUS, EPSILON_RESAMPLE
+from pyresample import kd_tree, geometry, image
+
+import numpy as np
+
+class Swath:
+    def __init__(
+        self,
+        cfg: DictConfig,
+        one_track_data: ExtractedHDF5Data,
+        band_label: str,
+    ):
+        #self.inc_angle = 0
+        self.cfg = cfg
+        self.one_track_data = one_track_data
+        self.chosen_band_data = one_track_data.Ka if band_label == 'Ka' else one_track_data.Ku
+        #breakpoint()
+        self.mask = []
+        self.index_mask = []
+        self.ice_conc_DPR = []
+
+    def Resample_ice_conc_DPR( 
+        self,
+        #cfg: DictConfig,        
+    ) -> None:
+        
+        DPR_swath_def = geometry.SwathDefinition(lons = self.chosen_band_data.latitude, 
+                                                    lats = self.chosen_band_data.longitude)
+        GMI_swath_def = geometry.SwathDefinition(lons = self.one_track_data.latitude_GMI, 
+                                                    lats = self.one_track_data.longitude_GMI)
+
+        self.ice_conc_DPR = kd_tree.resample_nearest(GMI_swath_def, self.one_track_data.ice_concentration_GMI,
+        DPR_swath_def, fill_value = FILL_VALUE_RESAMPLE, radius_of_influence = INFL_RADIUS, epsilon = EPSILON_RESAMPLE, nprocs=1)
+        #breakpoint()
+        
+    def Create_mask(
+        self,
+    ) -> None:
+
+        surface_type = self.chosen_band_data.surface_type
+        latitude = self.chosen_band_data.latitude
+        longitude = self.chosen_band_data.longitude
+
+        mask_no_land = np.logical_or(np.logical_and(surface_type >= 0,surface_type <= 99),                  # open ocean
+                                    np.logical_and(surface_type >= 300,surface_type <= 399))               # internal waters
+
+        mask_no_rain = self.chosen_band_data.precipitation_rate < 1
+        mask_no_ice = self.ice_conc_DPR < 0.01
+        mask_geo = np.logical_and.reduce((self.chosen_band_data.latitude >= self.cfg.region_of_interest.latitude_min,
+                                        self.chosen_band_data.latitude <= self.cfg.region_of_interest.latitude_max,  
+                                        self.chosen_band_data.longitude >= self.cfg.region_of_interest.longitude_min,
+                                        self.chosen_band_data.longitude <= self.cfg.region_of_interest.longitude_max,         
+                                        ))
+
+        self.mask = np.logical_and.reduce((mask_no_land, mask_no_rain, mask_no_ice, mask_geo))
+        self.index_mask = np.argwhere(self.mask)
+        
+
+    def Select_window(
+        self,
+        window_center,
+    ) -> np.dnarray:
+        
+
+
+    # def Select_window:
